b73e891022ab7156121cd36f44563a2e9be9f0a2
Abhijeet Singh <106686793+abhijeetsinghCXT@users.noreply.github.com>
2024-06-16 15:51:43 +0530

    Create Insertion.c

---
+++ DSA Code/Linked List/Insertion.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Create Node
+struct Node
+{
+    int data;
+    struct Node *link;
+};
+
+// Traverse and Print func()
+void printLL(struct Node *ptr)
+{
+
+    while (ptr != NULL)
+    {
+        printf("%d\n", ptr->data);
+        ptr = ptr->link;
+    }
+};
+
+
+//Insertion at Start of Linked List
+struct Node* InsertAtStart(struct Node *head, int data){
+struct Node *ptr = (struct Node *)malloc(sizeof(struct Node));
+ptr->link = head;
+ptr->data = data;
+head = ptr;
+};
+
+int main()
+{
+
+    struct Node *head, *node2, *node3;
+    // Dynamic Memory Allocation in Heap
+    head = (struct Node *)malloc(sizeof(struct Node));
+    node2 = (struct Node *)malloc(sizeof(struct Node));
+    node3 = (struct Node *)malloc(sizeof(struct Node));
+
+    // Initialize data of Node
+    head->data = 10;
+    head->link = node2;
+
+    node2->data = 39;
+    node2->link = node3;
+
+    node3->data = 52;
+    node3->link = NULL;
+
+    
+    head = InsertAtStart(head, 7);
+    printLL(head);
+
+    return 0;
+}
